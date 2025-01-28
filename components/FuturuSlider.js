321bf27dfd67acc25e79f3c19ab816215f9a2abd
Erfan <erfuun.mi82@gmail.com>
2025-01-28 08:35:24 +0330

    add new project page

---
+++ components/FuturuSlider.js
@@ -0,0 +1,94 @@
+// data
+const workSlides = {
+    slides: [
+      {
+        images: [
+          {
+            title: 'title',
+            path: '/TestPage.png',
+          },
+          {
+            title: 'title',
+            path: '/ComparePage.jpg',
+          },
+
+        ],
+      },
+
+      {
+        images: [
+            {
+                title: 'title',
+                path: '/Comparepage2.png',
+              },
+              {
+                title: 'title',
+                path: '/PredictionPage.png',
+              },
+        ],
+      },
+
+    ],
+  };
+  
+  
+  
+  
+
+  
+  
+  // data
+  
+  import { Swiper, SwiperSlide } from 'swiper/react';
+  import 'swiper/css';
+  import 'swiper/css/free-mode';
+  import 'swiper/css/pagination';
+  
+  import { pagination } from 'swiper';
+  
+  import { BsArrowRight } from 'react-icons/bs'
+  
+  import Image from 'next/image';
+import { SiDjango } from 'react-icons/si';
+import { FaPython } from 'react-icons/fa';
+  const FuturuSlider = () => {
+    return (
+      <Swiper
+        spaceBetween={10}
+        pagination={{
+          clickable: true,
+        }}
+        //  modules={[ pagination]}
+        className='h-[480px] sm:h[480px]  mt-[400px] lg:mt-20'
+      >
+        {
+          workSlides.slides.map((slide, index) => {
+  
+            return (
+              <SwiperSlide key={index}>
+                <div className='grid grid-cols-1 grid-row-1 mt-10 gap-4 cursor-pointer '>
+                  {slide.images.map((image, index) => {
+                    return <div className='relative rounded-lg overflow-hidden flex items-center justify-center 
+                    group' key={index}>
+                      <div className='flex items-center justify-center relative overflow-hidden group '>
+                        <div className='md:h-[200px] h-full'>
+                          <Image src={image.path} width={500} height={200} alt='' />
+                        </div>
+
+
+                      
+                      
+                      </div>
+                    </div>
+                  })}
+                </div>
+              </SwiperSlide>
+            );
+          })
+        }
+      </Swiper>
+    );
+  };
+  
+  export default FuturuSlider;
+  
---
+++ pages/futuru-proj/index.js
@@ -0,0 +1,161 @@
+
+
+import WorkSlider from '../../components/WorkSlider';
+import Bulb from '../../components/Bulb';
+import Circles from '../../components/Circles';
+
+import { motion } from 'framer-motion'
+import { fadeIn } from '../../variants'
+import FuturuSlider from '../../components/FuturuSlider';
+
+
+import {
+
+  FaJs,
+  FaReact,
+  FaWordpress,
+  FaFigma,
+  FaMicrosoft,
+  FaPython,
+
+
+} from 'react-icons/fa'
+
+import {
+  SiNextdotjs,
+  SiCsharp,
+  SiCplusplus,
+  SiMongodb,
+  SiReactquery,
+  SiMicrosoftsqlserver,
+  SiRedux,
+  SiLinux,
+  SiDotnet,
+  SiBlazor,
+  SiFramer,
+  SiAdobephotoshop,
+  SiTailwindcss,
+  SiDjango,
+  SiThreedotjs,
+
+} from 'react-icons/si';
+import { useState } from 'react';
+
+
+export const aboutData = [
+  {
+    title: 'skills',
+    info: [
+      {
+        title: 'Back: ',
+        icons: [
+
+          <SiDjango key={2} />,
+          <FaPython key={2} />,
+          <SiMongodb key={2} />,
+
+        
+
+
+        ],
+      },
+      {
+        title: 'Front:',
+        icons: [
+          
+          
+          <SiNextdotjs key={2} />,
+          <SiThreedotjs key={2}/> ,
+          <SiReactquery key={2} />,
+          <SiRedux key={2} />,          
+          <SiTailwindcss key={2} />,
+         
+         
+          ],
+
+      },
+
+    ],
+  },
+
+
+
+];
+
+const Work = () => {
+
+    const [index, setIndex] = useState(0);
+
+  return (
+    <div className='h-full bg-primary/30 py-36  flex items-center'>
+    <Circles />
+    <div className=' container mx-auto'>
+      <div className='flex flex-col xl:flex-row gap-x-8'>
+      <motion.div variants={fadeIn('down' , 0.6)} initial='hidden' animate='show' exit='hidden'
+       className=' w-full  xl:max-w-[50%] -mt-[7%]'>
+       <FuturuSlider />
+       </motion.div>
+        {/* <div className=' text-center flex xl:w-[30vw] flex-col lg:text-left mt-[300px] -mb-[350px]  xl:mb-0 xl:mt-40 '>
+          <motion.h2 variants={fadeIn('up' , 0.2)} initial='hidden' animate='show' exit='hidden'
+           className='h2 xl:mt-8'>My Projects<span className='text-accent '></span></motion.h2>
+          <motion.p variants={fadeIn('up' , 0.4)} initial='hidden' animate='show' exit='hidden' 
+          className='mb-4 max-w-[400px] mx-auto lg:mx-0'>You can have look on some of my last Projection and there is source code for some of them in my github.</motion.p>
+        </div> */}
+
+<div class="md:flex-1 px-4 mt-[8%]">
+                <h2 class="text-2xl font-bold text-white mb-2">FuturU Web App</h2>
+                <p class="text-gray-300 text-sm mb-4">
+              3D sccaner app wich will help users to manage their 3D body scans
+                </p>
+       
+                <div class="mb-4">
+                  
+                           {aboutData[index].info.map((item, itemIndex) => {
+                             return (
+                               <div key={itemIndex} className='flex-1 flex flex-col md:flex-row max-w-max gap-x-2 items-center text-white/60 mb-2 mt-2 '>
+                                 <div className='text-white mb-2 mr-2 md:mb-0'>{item.title}</div>
+                                 <div className=' hidden md:flex'></div>
+                                 <div>{item.stage}</div>
+                                 <div className=' flex gap-x-4 '>
+               
+                                   {item.icons?.map((icon, itemIndex) => {
+                                     return <div className='text-2xl text-white' key={itemIndex}>{icon}</div>
+                                   })}
+               
+               
+                                 </div>
+                               </div>
+                             );
+                           })}
+                </div>
+
+                <div class="flex mb-4">
+                    <div class="mr-4">
+                        <span class="font-bold text-white">Publish date: </span>
+                        <span class="text-gray-300"> 2024/10/24</span>
+                    </div>
+              
+                </div>
+
+
+
+                <div>
+                    
+                    <p class="text-gray-300 text-sm mt-2">
+                    The FuturU app was a web application developed in collaboration with FuturU and Payatek to view and manage scans made by the PT-3D device.
+                    The app is able to predict the shape and structure of individuals' bodies by running an algorithm based on weight and muscle mass.
+                    </p>
+                </div>
+            </div>
+  
+      </div>
+    </div>
+      <Bulb />
+
+      </div>);
+
+    
+ 
+};
+
+export default Work;
