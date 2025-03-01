bc801bab8fc20455e3da9ce09bb0c1c20e65ebba
Erfan <erfuun.mi82@gmail.com>
2025-03-01 06:05:14 +0330

    add my own socials links

--- components/Socials.js
+++ components/Socials.js
@@ -1,5 +1,4 @@
 import Link from "next/link";
-
 import {
   RiTelegramLine,
   RiInstagramLine,
@@ -7,28 +6,27 @@ import {
   RiLinkedinLine,
   RiGithubLine,
   RiPinterestLine,
-} from 'react-icons/ri'
+} from 'react-icons/ri';
 
 const Socials = () => {
   return (
-  <div className=' flex items-center gap-x-5 text-lg -mt-[500px] sm:mt-0 xl:mt-10 '>
-    <Link href={''} className='hover:text-accent transition-all duration-300'>
-      <RiTelegramLine />
-    </Link>
-    <Link href={''} className='hover:text-accent transition-all duration-300'>
-      <RiInstagramLine />
-    </Link>
-    <Link href={''} className='hover:text-accent transition-all duration-300'>
-      <RiMailLine />
-    </Link>
-    <Link href={''} className='hover:text-accent transition-all duration-300'>
-      <RiGithubLine />
-    </Link>
-    <Link href={''} className='hover:text-accent transition-all duration-300'>
-      <RiLinkedinLine />
-    </Link>
-   
-  </div>
+    <div className='flex items-center gap-x-5 text-lg -mt-[500px] sm:mt-0 xl:mt-10'>
+      <Link href={'https://t.me/lO_ol32'} className='hover:text-accent transition-all duration-300'>
+        <RiTelegramLine />
+      </Link>
+      <Link href={'https://www.instagram.com/erfanm.fk'} className='hover:text-accent transition-all duration-300'>
+        <RiInstagramLine />
+      </Link>
+      <Link href={'mailto:erfuun.mi82@gmail.com'} className='hover:text-accent transition-all duration-300'>
+        <RiMailLine />
+      </Link>
+      <Link href={'https://github.com/Erfuunm'} className='hover:text-accent transition-all duration-300'>
+        <RiGithubLine />
+      </Link>
+      <Link href={'https://www.linkedin.com/in/erfan-mizban-b710631b7/'} className='hover:text-accent transition-all duration-300'>
+        <RiLinkedinLine />
+      </Link>
+    </div>
   );
 };
 
