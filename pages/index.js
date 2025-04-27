bbc8a11ce0c7eaeed7ece1bab5e7b4351bab74fc
kevin.reyes <kevin.reyes@blueagilis.com>
2025-04-27 10:48:57 +0330

    edit main page design

--- pages/index.js
+++ pages/index.js
@@ -10,14 +10,17 @@ import { motion } from "framer-motion";
 
 import { fadeIn } from '../variants';
 import Test from "./about/Test";
+
+
 const Home = () => {
   return( <div className='bg-primary/60 h-full '>
     <Test />
+    
 
-    <div className='w-full h-full -mt-0  bg-gradient-to-r from-primary/10 via-black/30 to-black/10 '>
+    <div className='w-full h-screen   bg-gradient-to-r from-primary/10 via-black/30 to-black/10 '>
       <div className='text-center flex flex-col justify-center xl:pt-40  xl:text-left h-full container mx-auto'>
 
-        <motion.h1 variants={fadeIn('down' , 0.2)} initial='hidden' animate='show' exit='hidden' className='h1  '> Time For Migration <br />  Into <span className=' text-blue-500'>Digital Reality :)</span></motion.h1>
+        <motion.h1 variants={fadeIn('down' , 0.2)} initial='hidden' animate='show' exit='hidden' className='h1 mt-[30%] sm:mt-[10%] xl:mt-[0%]  '> Time For Migration <br />  Into <span className=' text-blue-500'>Digital Reality :)</span></motion.h1>
         <motion.p variants={fadeIn('down' , 0.3)} initial='hidden' animate='show' exit='hidden' className='max-w-sm xl:max-w-xl pb-[50px] mx-auto xl:mx-0 mb-10 xl:mb-16'>
           Hi, Im Erfan Mizban .I study computer engineering at Ferdowsi university of mashhad .
           i realy like Programming and creating something new . i am the founder of NORD GROUP wich is a Student programming team.
