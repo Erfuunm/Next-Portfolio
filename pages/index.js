768f31190de7be57f983376b6bcf06f55ca78668
kevin.reyes <kevin.reyes@blueagilis.com>
2025-04-27 11:22:22 +0330

    edit main page text

--- pages/index.js
+++ pages/index.js
@@ -22,12 +22,12 @@ const Home = () => {
 
         <motion.h1 variants={fadeIn('down' , 0.2)} initial='hidden' animate='show' exit='hidden' className='h1 mt-[45%] sm:mt-[10%] xl:mt-[0%]  '> Time For Migration <br />  Into <span className=' text-blue-500'>Digital Reality :)</span></motion.h1>
         <motion.p variants={fadeIn('down' , 0.3)} initial='hidden' animate='show' exit='hidden' className='max-w-sm xl:max-w-xl pb-[50px] mx-auto xl:mx-0 mb-10 xl:mb-16'>
-        Hello, I'm Erfan Mizban
+        Hello, Im Erfan Mizban
 Computer Engineering student at Ferdowsi University of Mashhad | Full-Stack Developer | Founder of NORD GROUP
 
-I'm passionate about building innovative solutions through code, with professional experience in both frontend and backend development. As the founder of NORD GROUP, I lead a team of student developers working on impactful projects.
+Im passionate about building innovative solutions through code, with professional experience in both frontend and backend development. As the founder of NORD GROUP, I lead a team of student developers working on impactful projects.
 
-I'm always open to interesting collaborations and new opportunities. Let's connect and explore how we can work together!
+Im always open to interesting collaborations and new opportunities. Lets connect and explore how we can work together!
          
         </motion.p >
 
