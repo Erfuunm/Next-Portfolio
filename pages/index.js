0875b65b2e1d5e373e3c656e5b7e5de6f8370a39
kevin.reyes <kevin.reyes@blueagilis.com>
2025-04-27 11:01:10 +0330

    edit main page text

--- pages/index.js
+++ pages/index.js
@@ -20,12 +20,14 @@ const Home = () => {
     <div className='w-full h-screen   bg-gradient-to-r from-primary/10 via-black/30 to-black/10 '>
       <div className='text-center flex flex-col justify-center xl:pt-40  xl:text-left h-full container mx-auto'>
 
-        <motion.h1 variants={fadeIn('down' , 0.2)} initial='hidden' animate='show' exit='hidden' className='h1 mt-[30%] sm:mt-[10%] xl:mt-[0%]  '> Time For Migration <br />  Into <span className=' text-blue-500'>Digital Reality :)</span></motion.h1>
+        <motion.h1 variants={fadeIn('down' , 0.2)} initial='hidden' animate='show' exit='hidden' className='h1 mt-[45%] sm:mt-[10%] xl:mt-[0%]  '> Time For Migration <br />  Into <span className=' text-blue-500'>Digital Reality :)</span></motion.h1>
         <motion.p variants={fadeIn('down' , 0.3)} initial='hidden' animate='show' exit='hidden' className='max-w-sm xl:max-w-xl pb-[50px] mx-auto xl:mx-0 mb-10 xl:mb-16'>
-          Hi, Im Erfan Mizban .I study computer engineering at Ferdowsi university of mashhad .
-          i realy like Programming and creating something new . i am the founder of NORD GROUP wich is a Student programming team.
-          and i was work as Frontend and Backend programmer in many companies . if you want us to have cooperation 
-          just text me.
+        Hello, I'm Erfan Mizban
+Computer Engineering student at Ferdowsi University of Mashhad | Full-Stack Developer | Founder of NORD GROUP
+
+I'm passionate about building innovative solutions through code, with professional experience in both frontend and backend development. As the founder of NORD GROUP, I lead a team of student developers working on impactful projects.
+
+I'm always open to interesting collaborations and new opportunities. Let's connect and explore how we can work together!
          
         </motion.p >
 
